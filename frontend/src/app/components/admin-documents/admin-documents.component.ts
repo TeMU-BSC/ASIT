7cebeb5c6970cd96f3109b7ae36850b692be0013
Darryl Estrada <darrylestrada97@gmail.com>
2021-05-03 15:46:26 +0200

    feat: "Filters working for document list."

--- frontend/src/app/components/admin-documents/admin-documents.component.ts
+++ frontend/src/app/components/admin-documents/admin-documents.component.ts
@@ -18,6 +18,11 @@ export class AdminDocumentsComponent implements OnInit {
   loading: boolean
 
   //Form variables
+  idFilter: String;
+  abstractFilter: String;
+  titleFilter: String;
+  typeFilter: String;
+  sourceFilter: String;
   public documents: Document[];
   public selectedDocument: Document;
   public documentsFiltered: Document[]=[];
@@ -49,6 +54,89 @@ export class AdminDocumentsComponent implements OnInit {
 
 
   }
+
+
+  filter(){
+    this.documentsFiltered = this.documents.
+      filter(document => {
+        let idValid: boolean = false;
+        let abstractValid: boolean = false;
+        let titleValid: boolean = false;
+        let typeValid: boolean = false;
+        let sourceValid: boolean = false;
+
+
+
+        // IndexOf: Returns the position of the
+        // first occurrence of a substring.
+        // Otherwise returns -1
+        if(this.idFilter && this.idFilter!=""){
+          if(document.identifier.toLowerCase().indexOf
+          (this.idFilter.toLowerCase())!=-1){
+            idValid=true;
+          }
+        }else{
+          idValid=true;
+        }
+
+        // IndexOf: Returns the position of the
+        // first occurrence of a substring.
+        // Otherwise returns -1
+        if(this.abstractFilter && this.abstractFilter!=""){
+          if(document.abstract.toLowerCase().indexOf
+          (this.abstractFilter.toLowerCase())!=-1){
+            abstractValid=true;
+          }
+        }else{
+          abstractValid=true;
+        }
+
+          // IndexOf: Returns the position of the
+        // first occurrence of a substring.
+        // Otherwise returns -1
+        if(this.titleFilter && this.titleFilter!=""){
+          if(document.title.toLowerCase().indexOf
+          (this.titleFilter.toLowerCase())!=-1){
+            titleValid=true;
+          }
+        }else{
+          titleValid=true;
+        }
+
+          // IndexOf: Returns the position of the
+        // first occurrence of a substring.
+        // Otherwise returns -1
+        if(this.typeFilter && this.typeFilter!=""){
+          if(document.type.toLowerCase().indexOf
+          (this.typeFilter.toLowerCase())!=-1){
+            typeValid=true;
+          }
+        }else{
+          typeValid=true;
+        }
+
+          // IndexOf: Returns the position of the
+        // first occurrence of a substring.
+        // Otherwise returns -1
+        if(this.sourceFilter && this.sourceFilter!=""){
+          if(document.source.toLowerCase().indexOf
+          (this.sourceFilter.toLowerCase())!=-1){
+            sourceValid=true;
+          }
+        }else{
+          sourceValid=true;
+        }
+
+
+
+        return idValid && abstractValid && titleValid && typeValid && sourceValid;
+
+      })
+
+
+  }
+
+
 
   onSelect(document: Document): void {
     this.selectedDocument = document;
